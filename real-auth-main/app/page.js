af06c91e6497ca29685857007ce604780bf26cc5
hp <sdsoni2042@gmail.com>
2024-10-10 22:52:43 +0530

    zip pasted 3commit

---
+++ real-auth-main/app/page.js
@@ -0,0 +1,35 @@
+// "use client"
+// import { useSession, signIn, signOut } from "next-auth/react"
+
+// export default function Component() {
+  // const { data: session } = useSession()
+  // // console.log(session);
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session.user.email} <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   )
+  // }
+  // return (
+  //   <>
+  //     Not signed in <br />
+  //     <button onClick={() => signIn("github")}>Sign in Using Github</button>
+  //     <button onClick={() => signIn("google")}>Sign in Using google</button>
+  //   </>
+  // )
+// }
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import OlaMaps from "./component/OlaMaps";
+// import Header from "@/components/Header";
+export default function Home() {
+  return (
+    <div>
+      {/* <Header/> */}
+      <OlaMaps/>
+    </div>
+  );
+}
