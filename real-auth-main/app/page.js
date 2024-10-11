253364295b10e8a5ab691bd7d3963d410783a557
hp <sdsoni2042@gmail.com>
2024-10-11 15:43:55 +0530

    homepage directly mapcode

--- real-auth-main/app/page.js
+++ real-auth-main/app/page.js
@@ -1,35 +1,52 @@
-// "use client"
-// import { useSession, signIn, signOut } from "next-auth/react"
+"use client"
 
-// export default function Component() {
-  // const { data: session } = useSession()
-  // // console.log(session);
-  // if (session) {
-  //   return (
-  //     <>
-  //       Signed in as {session.user.email} <br />
-  //       <button onClick={() => signOut()}>Sign out</button>
-  //     </>
-  //   )
-  // }
-  // return (
-  //   <>
-  //     Not signed in <br />
-  //     <button onClick={() => signIn("github")}>Sign in Using Github</button>
-  //     <button onClick={() => signIn("google")}>Sign in Using google</button>
-  //   </>
-  // )
-// }
+import { useState } from "react";
+import DeckGL from "@deck.gl/react";
+import maplibregl from "maplibre-gl";
+import StaticMap from 'react-map-gl'; // Ensure this matches the library you are using
+import "mapbox-gl/dist/mapbox-gl.css";
 
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import OlaMaps from "./component/OlaMaps";
-// import Header from "@/components/Header";
+
+const OlaMaps = () => {
+  const [viewState, setViewState] = useState({
+    longitude: 76.3433,
+    latitude: 19.8762,
+    zoom: 10,
+  });
+
+  return (
+    <div className="flex relative ">
+      <DeckGL
+        style={{ width: "50vw", height: "80vh", overflow: "hidden", position:"absolute", right:"0"}}
+        viewState={viewState}
+        onViewStateChange={({ viewState }) => setViewState(viewState)}
+        controller={true}
+        layers={[]}
+      >
+        <StaticMap
+          mapLib={maplibregl}
+          mapStyle="https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json"
+          transformRequest={(url, resourceType) => {
+            if (url.includes("?")) {
+              url = url + "&api_key=OCT8Oo19bcr3JkkGFuUZLDe4VW4wOXM1blmHWFSa";
+            } else {
+              url = url + "?api_key=OCT8Oo19bcr3JkkGFuUZLDe4VW4wOXM1blmHWFSa";
+            }
+            return { url, resourceType };
+          }}
+        />
+      </DeckGL>
+    </div>
+  );
+};
+
+
+// import OlaMaps from "./component/OlaMaps";
+
 export default function Home() {
   return (
     <div>
-      {/* <Header/> */}
-      <OlaMaps/>
+      <OlaMaps />
     </div>
   );
 }
